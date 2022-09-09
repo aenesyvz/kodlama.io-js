1dc68eef14b0d02010b0301c81302d55f5f4dd9a
aenesyvz <72816920+aenesyvz@users.noreply.github.com>
2022-09-09 17:26:21 +0300

    Add files via upload

---
+++ jscamp-master/src/app.js
@@ -0,0 +1,34 @@
+console.log("Merhaba Kodlama.io")
+
+
+let dolarBugun = 9.30
+
+let dolarDun = 9.20
+dolarDun = "9.20"
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+
+
+console.log(euroDun)
+
+
+let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log("<ul>")
+for(let i = 0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+{/* <ul>
+    <li>Konut kredisi</li>
+    <li>Emlak Konut Kredisi</li>
+    <li>Kamu Konut Kredisi</li>
+</ul> */}
+
+console.log(konutKredileri)
